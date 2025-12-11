0437001717206a9e2c8c2831194c2549cee8c8cb
Diptayan Jash <dj2037@srmist.edu.in>
2025-12-11 17:17:50 +0530

    feat: Add privacy policy and support screens.

---
+++ recap/recap/views/shared/privaryPolicy.swift
@@ -0,0 +1,43 @@
+//
+//  privaryPolicy.swift
+//  recap
+//
+//  Created by Diptayan Jash on 11/12/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct privaryPolicy: View {
+    @State private var page = WebPage()
+    private let urlString = "https://recap.djdiptayan.in/privacyPolicy"
+    
+    var body: some View {
+        // NavigationStack {
+            ZStack {
+                WebView(page)
+                    .ignoresSafeArea(.all)
+                if page.isLoading {
+                    ProgressView(value: page.estimatedProgress)
+                        .progressViewStyle(.circular)
+                        .scaleEffect(1.5)
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(10)
+                }
+            }
+            .navigationTitle("Privacy Policy")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                if let url = URL(string: urlString) {
+                    page.load(URLRequest(url: url))
+                } else {
+                    print("Error: Invalid URL")
+                }
+            }
+        // }
+    }
+}
+
+#Preview {
+    privaryPolicy()
+}
---
+++ recap/recap/views/shared/support.swift
@@ -0,0 +1,43 @@
+//
+//  support.swift
+//  recap
+//
+//  Created by Diptayan Jash on 11/12/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct support: View {
+    @State private var page = WebPage()
+    private let urlString = "https://recap.djdiptayan.in/support"
+    
+    var body: some View {
+        // NavigationStack {
+            ZStack {
+                WebView(page)
+                    .ignoresSafeArea(.all)
+                if page.isLoading {
+                    ProgressView(value: page.estimatedProgress)
+                        .progressViewStyle(.circular)
+                        .scaleEffect(1.5)
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(10)
+                }
+            }
+            .navigationTitle("Support")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                if let url = URL(string: urlString) {
+                    page.load(URLRequest(url: url))
+                } else {
+                    print("Error: Invalid URL")
+                }
+            }
+        // }
+    }
+}
+
+#Preview {
+    support()
+}
